f054ffa3a706a31fe80c183c297c2c4ede5fab9a
MixaiLOLomonosov <mixailblade@gmail.com>
2024-11-22 10:28:32 +0300

    Add files via upload

---
+++ Peremennie.py
@@ -0,0 +1,8 @@
+course = "Pithon"
+total = "3"
+horse_spend = "1.5"
+time_for_one_tasks = "0.5"
+print("corse Pithon")
+print("total=3")
+print("horse_name=1.5 horse")
+print("time_for_one_tasks=0.5 horse")
